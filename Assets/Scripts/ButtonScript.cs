92849a36f15e63e0c34c466dccace6f2806ae59c
LAPTOP-S122I8BH\Kim Marzo <kjrmarzo@gmail.com>
2018-09-29 14:03:48 -0400

    Main menu base (without art)

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            
+    }
+    public void Play()
+    {
+        Debug.Log("Test Play");
+        //SceneManager.LoadScene(gameplay);
+    }
+    public void Credits()
+    {
+        Debug.Log("Test Credits");
+        //SceneManager.LoadScene(credits);
+    }
+    public void Quit()
+    {
+        Debug.Log("Test Quit");
+        Application.Quit();
+       
+    }
+}
+ 
+
