eba1227937dfbc237c72946a8efb46a8698490de
Michael Zatlin <michael.zatlin@yahoo.com>
2018-09-29 12:43:46 -0700

    Very basic player jump and movement

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -3,25 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour {
-
+    Rigidbody2D rb;
+    int movespeed = 20;
+    [SerializeField]
+    Vector2 Jumpdirection;
+    bool isgrounded;
     // Use this for initialization
     void Start()
     {
         Debug.Log(PlayerManager.player.Health);
+        rb = GetComponent<Rigidbody2D>();
+        Jumpdirection = new Vector2(0, 30);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Debug.Log("Jump");
+        moveplayer();
 
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) { 
+
+            rb.AddForce(Jumpdirection * 10);
+            isgrounded = false;
         }
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
             Debug.Log("Crouch");
         }
+
+    }
+    void moveplayer()
+    {
+        rb.velocity = new Vector2(1*movespeed*Time.deltaTime, 0);
     }
 }
 
