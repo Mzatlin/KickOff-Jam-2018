066761a4c764efbd9cf3f43663d810cb96a8ea2f
Michael Zatlin <michael.zatlin@yahoo.com>
2018-09-29 12:06:04 -0700

    Created Player Movement Controller

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour {
-    public PlayerManager player;
+    public static PlayerManager player;
     private int health = 5;
     void Awake()
     {
---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log(PlayerManager.player.Health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("Jump");
+
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Debug.Log("Crouch");
+        }
+    }
+}
+
