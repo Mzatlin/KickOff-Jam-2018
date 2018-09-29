5bb5d104b9d38d2e5448745c4f6f2248043a306b
Kim Marzo <kjrmarzo@gmail.com>
2018-09-29 17:14:03 -0400

    + Running and Crouching animation

--- Assets/PlayerCrouch.cs
+++ Assets/PlayerCrouch.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class PlayerCrouch : MonoBehaviour {
     BoxCollider2D collider;
     Vector2 size;
+    public Animator animator;
     // Use this for initialization
     void Start () {
         collider = GetComponent<BoxCollider2D>();
         size = collider.size;
+
     }
 	
 	// Update is called once per frame
 	void Update () {
+        
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
             Debug.Log("Crouch");
             collider.size = new Vector2(.1f, .1f);
+            animator.SetBool("isCrouching", true);
         }
         if (Input.GetKeyUp(KeyCode.Mouse1))
         {
             Debug.Log("UnCrouch");
             collider.size = size;
+            animator.SetBool("isCrouching", false);
         }
     }
 }
