58167785fe4d566efc0222bb8fc34d5741589eb5
Michael Zatlin <michael.zatlin@yahoo.com>
2018-09-29 19:54:20 -0700

    Made exit

---
+++ Assets/Exit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Exit : MonoBehaviour {
+    PlayerManager m_player;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        m_player = collision.gameObject.GetComponent<PlayerManager>();
+        if (m_player != null)
+        {
+            Debug.Log("Touched!");
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
